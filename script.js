52dabbc14ee7fc3d38d0c98f4002c1b1cc737546
Simon Boijer <boijersi@arcada.fi>
2025-01-29 17:00:51 +0200

    Refactored, validation, change grid size button

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-const numberOfGridCells = 16;
+let GRID_SIZE = 16;
 
 function generateGrid() {
     const fragment = document.createDocumentFragment();
     const gridContainer = document.querySelector(".grid-container");
+    gridContainer.innerHTML = "";
 
-    for (let i = 0; i < numberOfGridCells; i++) {
+    for (let row = 0; row < GRID_SIZE; row++) {
         const gridRow = document.createElement("div")
         gridRow.className = "grid";
         gridContainer.appendChild(gridRow);
 
-        for (let i = 0; i < numberOfGridCells; i++) {
+        for (let col = 0; col < GRID_SIZE; col++) {
             const cell = document.createElement("div");
-            cell.style.width = "50px";
-            cell.style.height = "50px";
-            cell.style.backgroundColor = "blue";
-            cell.style.flex = "1";
-            cell.style.margin = "2px";
+            cell.className = "cell"
+
             fragment.appendChild(cell);
+            cell.addEventListener("mouseover", () => {
+                changeColorOnMouseOver(cell);
+            })
         }
         gridRow.appendChild(fragment);
     }
 }
 generateGrid()
+
+function changeColorOnMouseOver(cell) {
+    cell.style.backgroundColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    setTimeout(() => {
+        cell.style.backgroundColor = "blue";
+    }, 1000)
+}
+
+function changeGridSize() {
+    GRID_SIZE = prompt("Enter value");
+    if(!isInt(GRID_SIZE)) {
+        GRID_SIZE = 16;
+        alert("Not a number");
+    }
+    generateGrid();
+}
+
+function isInt(value) {
+    return !isNaN(value) && 
+           parseInt(Number(value)) == value && 
+           !isNaN(parseInt(value, 10));
+  }
